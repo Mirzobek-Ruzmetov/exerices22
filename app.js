5918234d1348ad51bae744864211ff7c1b1df4ad
Mirzobek Ruzmetov <ruzmetovmirzobek10@gmail.com>
2026-02-11 22:25:04 +0500

    app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const appName = "My App";
+console.log("Ilovaga xush kelibsiz!");
+
+let userName = "Ali";
+let age = 16;
+
+console.log("Salom, " + userName);
+console.log("Yoshingiz: " + age);
+
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let division = num1 / num2;
+
+console.log("Yig'indi: " + sum);
+console.log("Ayirma: " + difference);
+console.log("Ko'paytma: " + product);
+console.log("Bo'lish: " + division);
+
+console.log("Dastur tugadi.");
